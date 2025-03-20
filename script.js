2c30727de35a57ea1209ec1d83f9e3f82407d89f
Yasir Haider <104465310+ohammed09@users.noreply.github.com>
2025-03-20 13:36:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("الموقع جاهز!");
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.querySelector(".btn");
+    btn.addEventListener("mouseenter", () => {
+        btn.style.background = "#0056b3";
+        btn.style.color = "white";
+    });
+    btn.addEventListener("mouseleave", () => {
+        btn.style.background = "white";
+        btn.style.color = "#007bff";
+    });
 });
